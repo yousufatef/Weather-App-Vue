84d1404303d7eec80a1193c68f317e21da109e4d
youssef <youssefatef@gmail.com>
2024-05-28 03:12:04 +0300

    ..

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import { createApp } from "vue";
 import { createRouter, createWebHistory } from "vue-router";
 import "./assets/tailwind.css";
 import App from "./App.vue";
-import Home from "./views/Home.vue";
+import HomeView from "./views/HomeView.vue";
+import CityView from "./views/CityView.vue";
 
-const routes = [{ path: "/", name: "home", component: Home }];
+const routes = [
+  { path: "/", name: "home", component: HomeView },
+  { path: "/weather/:state/:city", name: "cityView", component: CityView },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
